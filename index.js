f7cd6aa0aad4c0c0bc0a0a534e6dbeb76b8ae569
Rubiidev / Circle IDE Owner <158863401+Rubiidev-18@users.noreply.github.com>
2024-03-13 20:28:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//Some basic scripts made from Js to work the website, and make it interactive.
+console.log('This part is used for devolopers.');
+
+function whatsNew() {
+    var News;
+    News = '<h2>Whats New?</h2><p>The bark editor is being created in.</p>';
+    document.getElementById("boxChanger").innerHTML = News;
+}
+
+function recentCom() {
+    var Commits;
+    Commits = '<h2>Recent Commits</h2><p id="latest">Loading...</p>';
+    document.getElementById("boxChanger").innerHTML = Commits;
+}
+
+function randomTxt() {
+    //geeksforgeeks.org for the random text generator.
+    var wrd;
+    let arr = ["Bark! woof!", "Bark is a mod of scratch with new extensions",
+    "Scratch dog", "Bark is being created, so stay patient!!", "Note: Bark does not support accounts yet", 
+    "Funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!", "Why not help with bark??", "A special version is here somewere..",
+    "Bark is hosted with github pages.", "Funfact: im a funfact!", "Bark is made from penguin mod made from turbowarp etc.", 
+    "Wanna help create bark? go <a href='Jobs.html' class='link'>Here</a>"];
+
+    wrd = (arr[(Math.floor(Math.random() * arr.length))]);
+    document.getElementById("randomWord").innerHTML = wrd;
+}
+
+function checkSignup() {
+let username = document.getElementById('username').value;
+if (username != '') {
+            document.getElementById('page').innerHTML = '<br/><h2>Welcome to Bark ' + username + '!</h2>';
+    }
+}
+
+function jobRegister() {
+    let job = document.querySelector('input[name=job]:checked').value;
+    let username = document.getElementById('github-username').value;
+    document.getElementById('page').innerHTML = '<br/><h2>You have selected a Job to ' + job + '</h2><h4>If your actually <em>'+ username +'</em></h4>';
+    }
